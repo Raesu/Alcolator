4a6c104155574d07d3bf98f522b5c416652a0030
Ryan Summe <ryan.summe@gmail.com>
2015-06-11 20:17:23 +0800

    Add calcs and whiskey controller

---
+++ Alcolator/Alcolator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  Alcolator
+//
+//  Created by Ryan Summe on 6/11/15.
+//  Copyright (c) 2015 Ryan Summe. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
