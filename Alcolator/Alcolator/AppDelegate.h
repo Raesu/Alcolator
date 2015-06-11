16e7d09df4de2a743cd75cf54e9a97061158e294
Ryan Summe <ryan.summe@gmail.com>
2015-06-11 18:32:23 +0800

    First commit

---
+++ Alcolator/Alcolator/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Alcolator
+//
+//  Created by Ryan Summe on 6/9/15.
+//  Copyright (c) 2015 Ryan Summe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Alcolator/Alcolator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alcolator
+//
+//  Created by Ryan Summe on 6/9/15.
+//  Copyright (c) 2015 Ryan Summe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
